6ccd26b538c81244d172ba71d9f8b25689ba5f63
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-06-09 17:18:17 -0500

    Updated Calculator app with some noice features. Added rubocop.yml config file.

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -10,46 +10,128 @@
 # perform the operation on the two numbers
 # output the result
 
-num1 = 0
-num2 = 0
+LANGUAGE = "en"
+require "yaml"
+MESSAGES = YAML.load_file("calculator_messages.yml")
 
-Kernel.puts("Welcome to Calculator!")
-
-loop do 
-  Kernel.puts("First number?")
-  num1 = Kernel.gets().chomp()
-  break if num1.to_i().to_s() == num1
-  Kernel.puts("Invalid input. Please enter a number.")
+def messages(message, lang="en")
+  MESSAGES[lang][message]
 end
 
-loop do
-  Kernel.puts("Second number?")
-  num2 = Kernel.gets().chomp()
-  break if num2.to_i().to_s() == num2
-  Kernel.puts("Invalid input. Please enter a number.")
-end
-
-loop do
-  Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-  operation = Kernel.gets().chomp().downcase()
-
-  if operation == "add" || operation == "1"
-    result = num1.to_i() + num2.to_i()
-    break Kernel.puts("Result: #{result}")
-  elsif operation == "subtract" || operation == "2"
-    result = num1.to_i() - num2.to_i()
-    break Kernel.puts("Result: #{result}")
-  elsif operation == "multiply" || operation == "3"
-    result = num1.to_i() * num2.to_i()
-    break Kernel.puts("Result: #{result}")
-  elsif operation == "divide" || operation == "4"
-    if num2.to_i() == 0
-      Kernel.puts("Cannot divide by 0. Please choose a different operation or restart with new numbers.")
-      next
-    end
-    result = num1.to_f() / num2.to_f
-    break Kernel.puts("Result: #{result}")
+def prompt(key)
+  message = messages(key, LANGUAGE)
+  if message.nil?
+    Kernel.puts("=> #{key}")
   else
-    Kernel.puts("Invalid operation.")
+    Kernel.puts("=> #{message}")
+  end
+end
+
+# original integer validation used
+
+# def number?(num)
+#   num.to_i().to_s() == num
+# end
+
+# refactored integer validation
+
+# def number?(num)
+#   num =~ /^-?\d+$/
+# end
+
+# refactored number validation to included floats
+
+def number?(num)
+  num =~ /^ *[+,-]?\d*\.?\d*([e,E]\d)? *$/ && num =~ /\d/
+  # first half of statement: matches optional preceding +/- sign,
+  # 0+ digits before decimal, optional decimal, 0+ digits after decimal
+  # second half verifies there is at least one digits in the input
+end
+
+def operation_to_message(operation)
+  message = case operation
+            when "1" then "Adding"
+            when "2" then "Subtracting"
+            when "3" then "Multiplying"
+            when "4" then "Dividing"
+            end
+  message
+end
+
+prompt("welcome")
+
+name = ""
+
+loop do
+  name = Kernel.gets().chomp()
+  break unless name.empty?()
+  prompt("valid_name")
+end
+
+prompt("Hi #{name}!")
+
+loop do
+  num1 = 0
+  num2 = 0
+
+  loop do
+    prompt("first_number_prompt")
+    num1 = Kernel.gets().chomp()
+    break if number?(num1)
+    prompt("invalid_number_error")
+  end
+
+  loop do
+    prompt("second_number_prompt")
+    num2 = Kernel.gets().chomp()
+    break if number?(num2)
+    prompt("invalid_number_error")
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+  prompt(operator_prompt)
+
+  operation = ""
+
+  loop do
+    operation = Kernel.gets().chomp().downcase()
+    break if %w(1 2 3 4).include?(operation)
+    prompt("invalid_operation_error")
+  end
+
+  prompt("#{operation_to_message(operation)} the two numbers...")
+
+  result = case operation
+           when "1" then num1.to_f() + num2.to_f()
+           when "2" then num1.to_f() - num2.to_f()
+           when "3" then num1.to_f() * num2.to_f()
+           when "4"
+             if num2.to_f() == 0
+               prompt("division_by_zero_error")
+               next
+             end
+             num1.to_f() / num2.to_f
+           end
+
+  prompt("Result: #{result}")
+
+  prompt("again")
+  again = ""
+
+  loop do
+    again = Kernel.gets().chomp().downcase()
+    break if again == "y" || again == "n"
+    prompt("again_error")
+  end
+
+  if again == "n"
+    prompt("quit")
+    break
   end
 end
--- lesson_2/pseudo_practice.rb
+++ lesson_2/pseudo_practice.rb
@@ -4,30 +4,25 @@ def summer(num1, num2)
   num1 + num2
 end
 
-# 2. a method that takes an array of strings, 
+# 2. a method that takes an array of strings,
 # and returns a string that is all those strings concatenated together
 
 def string_smasher(strings)
-  result_string = ""
-  strings.each { |string|  result_string += string }
+  result_string = ''
+  strings.each { |string| result_string += string }
   result_string
 end
 
-# 3. a method that takes an array of integers, 
+# 3. a method that takes an array of integers,
 # and returns a new array with every other element
 
 def every_other(numbers)
   arr = []
-  
-  numbers.each_with_index do |number, ind|
-    if ind % 2 == 1
-      arr.push(number)
-    end
-  end
+
+  numbers.each_with_index { |number, ind| arr.push(number) if ind.odd? }
 
   arr
 end
-
 
 # testing
 puts summer(1, 2)
@@ -35,6 +30,6 @@ puts summer(-1, 2)
 puts summer(-1, -2)
 puts summer(0, 0)
 
-p string_smasher(["hi", "my", "name", "is", "bob"])
+p string_smasher(%w(hi my name is bob))
 
 p every_other(Array(1..10))
