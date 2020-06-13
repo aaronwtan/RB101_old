aa2fad87eb54b3b396a060993e78c833b34b223e
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-06-12 22:48:23 -0500

    Add Easy 2 Lesson 3 Practice Problems

---
+++ lesson_3/easy2/ex_4.rb
@@ -0,0 +1,8 @@
+# See if the name "Dino" appears in the string below:
+
+advice = "Few things in life are as important as house training your pet Dinosaur."
+
+puts advice.include?("Dino")
+puts advice.match?("Dino")
+
+puts (/ Dino /).match(advice) != nil
